d597e106fb5e9883ef35bd4681a5d4516aad61cd
Maynor Omar Varela Mercado <varelamaynor02@gmail.com>
2026-03-24 08:04:00 -0600

    Creamos nuestro JS y agregamos las variable y el Alert

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Bienvenidos a la pagina del Colegio Cristiano Josue 1:8");
+let nombre = "Javier";
+let apellido = "Gonzalez";
+let edad = 25;
+let activo = true;
+let nulo = null;
+let indefinido = undefined;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(activo);
+console.log(nulo);
+console.log(indefinido);
